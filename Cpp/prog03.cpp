1d23d1b179b54afed7d55eec054153e3d43b3259
João L. R. Neto <joaolrneto@gmail.com>
2020-05-19 12:06:04 -0300

    testando o VS Code

---
+++ Cpp/prog03.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    system("cls");
+    float x,y,z;
+    cout<<"Entre com dois valores :";
+    cin>>x>>y;
+    z=x+y;
+    cout<<"Soma dos dois valores = "<<z<<endl;
+    system("pause");
+    return 0;
+}
